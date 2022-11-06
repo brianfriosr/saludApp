4372ff605d3f37a5323f2530b4e3966375938f7c
Brian Rios <78057691+brianfriosr@users.noreply.github.com>
2022-11-05 23:26:43 -0500

    home fragment improved and bottom menu in progress

--- app/src/main/java/com/rios/figueroa/saludappbc/view/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/rios/figueroa/saludappbc/view/ui/activities/HomeActivity.kt
@@ -2,11 +2,20 @@ package com.rios.figueroa.saludappbc.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import com.rios.figueroa.saludappbc.R
+import java.util.zip.Inflater
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater:MenuInflater=menuInflater
+        inflater.inflate(R.menu.menu_bottom,menu)
+        return true
     }
 }
