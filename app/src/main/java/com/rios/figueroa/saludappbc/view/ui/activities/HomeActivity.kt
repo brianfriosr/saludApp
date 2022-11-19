f055bf2509d98a6bf15449853340626e9771b391
psycodev <92122576+psycodev@users.noreply.github.com>
2022-11-18 20:06:18 -0500

    activar boton atras

--- app/src/main/java/com/rios/figueroa/saludappbc/view/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/rios/figueroa/saludappbc/view/ui/activities/HomeActivity.kt
@@ -18,8 +18,4 @@ class HomeActivity : AppCompatActivity() {
         inflater.inflate(R.menu.menu_bottom,menu)
         return true
     }
-
-    override fun onBackPressed() {
-        return
-    }
 }
